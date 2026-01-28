934d9e1ad7f1a9a4435c68e5c58d455ded3621e1
Sabbir-75 <mdsabbirhossain9200@gmail.com>
2026-01-29 03:21:39 +0600

    implementation Activity and Request section and  polishing

--- src/Components/Requests/Requests.jsx
+++ src/Components/Requests/Requests.jsx
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardContainer from '../Container/CardContainer/CardContainer';
 
+const repairData = [
+    {
+        id: 1,
+        title: "Screen Repair",
+        value: 156
+    },
+    {
+        id: 2,
+        title: "Battery Replacement",
+        value: 89
+    },
+    {
+        id: 3,
+        title: "Back Glass Repair",
+        value: 67
+    },
+    {
+        id: 4,
+        title: "Software Issues",
+        value: 45
+    },
+];
+
 const Requests = () => {
+    const maxValue = 230;
+    const [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+         setAnimate(true)
+    }, []);
+
     return (
-         <CardContainer>
+        <CardContainer>
             <h1 className='text-[#FFFFFF] text-xl font-normal'>Top Repair Requests</h1>
-            
-         </CardContainer>
+            <div className="space-y-5">
+                {repairData.map((item, index) => {
+                    const percentage = (item.value / maxValue) * 100;
+                    return (
+                        <div key={index}>
+                            <div className="flex justify-between text-white mb-2">
+                                <span>{item.title}</span>
+                                <span className="text-[#C0CCDD]">
+                                    {item.value} requests
+                                </span>
+                            </div>
+
+                            <div className="w-full h-2 bg-[#1D293D] rounded-full">
+                                <div
+                                    className="h-2 rounded-full bg-gradient-to-r from-[#2B7FFF] to-[#00B8DB] transition-all duration-700"
+                                    style={{
+                                        width: animate ? `${percentage}%` : '0%',
+                                    }}
+                                ></div>
+                            </div>
+                        </div>
+                    )
+                }
+                )}
+            </div>
+        </CardContainer>
     );
 };
 
